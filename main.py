dbe34034c890fa959c48c9a8be46e29b5b86f7d8
ViciousPanda <info@jacobgesink.com>
2021-12-24 15:02:22 +0100

    starting text and levels init

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from typing_extensions import runtime
+import time
 import pygame
 import os
-from tools import scale_image, blit_rotate_center
+from tools import scale_image, blit_rotate_center, blit_text_center
+
+pygame.font.init()
 
 """ Game Settings """
-
 # Center the Screen
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 
@@ -46,19 +48,53 @@ ENEMY_1 = pygame.transform.rotate(
     pygame.image.load(os.path.join("assets", "camera_002.png")), TURN
 )
 
+MAIN_FONT = pygame.font.SysFont("79", 44)
+
 # pygame.transform.rotate(x,y)S
 # pygame.transform.scale(x,y)
 # pygame.draw.rect(WIN, BLACK, 640, 360, 20, 20)
 
 
-class Abstract:
+# Levels
+class GameInfo:
+    LEVELS = 3
+
+    def __init__(self, level=1):
+        self.level = level
+        self.started = False
+        self.level_start_time = 0
+
+    def next_level(self):
+        self.level += 1
+        self.started = False
+
+    def reset(self):
+        self.level = 1
+        self.started = False
+        self.level_start_time = 0
+
+    def game_finished(self):
+        return self.level > self.LEVELS
+
+    def start_level(self):
+        self.started = True
+        self.level_start_time = time.time()
+
+    def get_level_time(self):
+        if not self.started:
+            return 0
+        return self.level_start_time - time.time
+
+
+# class enemies
+class Abstract_Enemy:
     def __init__(self, max_vel, rotation_vel):
         self.img = self.IMG
         self.max_vel = max_vel
         self.vel = 0
         self.rotation_vel = rotation_vel
         self.angle = turn_angle
-        self.x, self.y = self.START_POS
+        self.x, self.y = self.ENEMY_START_POS
 
     def rotate(self, left=False, right=False):
         if left:
@@ -70,22 +106,32 @@ class Abstract:
         blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
 
 
-# class Player:
-class player(Abstract):
-    IMG = PLAYER
-    START_POS = (50, 290)
+class enemy_camera(Abstract_Enemy):
+    IMG = ENEMY_1
+    ENEMY_START_POS = (190, 200)
+
+
+# class Player
+class player:
+    def __init__(self):
+        self.img = PLAYER
+        self.player_start_pos = (50, 290)
+        self.x, self.y = self.player_start_pos
+        self.angle = turn_angle
 
     # Collision
     def collide(self, mask, x=0, y=0):
-        player_mask = pygame.mask.from_surface(self.IMG)
+        player_mask = pygame.mask.from_surface(self.img)
         offset = (int(self.x - x), int(self.y - y))
         poi = mask.overlap(player_mask, offset)
         return poi
 
+    def reset(self):
+        # self.x, self.x = self.player_start_pos
+        self.x, self.y = (50, 290)
 
-class enemy_camera(Abstract):
-    IMG = ENEMY_1
-    START_POS = (190, 200)
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
 
 
 # Draw objects
@@ -133,24 +179,39 @@ def player_movement(keys_pressed, player_o):
 
 def main():
     images = [(BACKGROUND, (0, 0)), (FINISH, FINISH_POS)]
-    player_o = player(4, 4)
+    player_o = player()
     enemy_o = enemy_camera(4, 4)
     clock = pygame.time.Clock()
+    game_info = GameInfo()
     run = True
+
     while run:
         clock.tick(FPS)
+
+        draw_window(WIN, images, player_o, enemy_o)
+
+        while not game_info.started:
+            blit_text_center(
+                WIN, MAIN_FONT, f"Press any key to start level {game_info.level}"
+            )
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    break
+                if event.type == pygame.KEYDOWN:
+                    game_info.start_level()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        # player_o.x += 1
 
         keys_pressed = pygame.key.get_pressed()
         player_movement(keys_pressed, player_o)
-        draw_window(WIN, images, player_o, enemy_o)
 
         if player_o.collide(FINISH_MASK, *FINISH_POS) != None:
-            print("FINISH")
+            player_o.reset()
 
     pygame.quit()
 
